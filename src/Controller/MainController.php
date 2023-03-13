6f148d343befb586b997c58a1fc1cbbf75f15a2c
vyacheslav.belchuk <vyacheslav.belchuk@gmail.com>
2023-03-14 00:59:55 +0300

    Fixed a name of main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HymnController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function index(): Response
